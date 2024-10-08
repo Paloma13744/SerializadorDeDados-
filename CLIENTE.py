29a0032ee1d80a64b01c30438d4fc9270cc6a73d
Paloma Nascimento <paloma13744@gmail.com>
2024-10-08 13:16:25 -0300

    Mudanças

--- CLIENTE.py
+++ CLIENTE.py
@@ -1,9 +1,16 @@
 import socket
-host = '127.0.0.1'     # Endereco IP do Servidor
-porta = 5000           # Porta que o Servidor esta
+
+host = '127.0.0.1'     # Endereço IP do Servidor
+porta = 5000           # Porta que o Servidor está
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 destino = (host, porta)
+
+# Conecta ao servidor
 soquete.connect(destino)
-soquete.send("Ola Mundo!")
+
+# Envia a mensagem (convertida para bytes)
+soquete.sendall(b"Ola Mundo!")  # Use b"" para enviar uma string como bytes
+
+# Fecha o socket
 soquete.close()
 
--- SERVIDOR.py
+++ SERVIDOR.py
@@ -1,15 +1,19 @@
 import socket
-host = '127.0.0.1' # Endereco IP do Servidor
-porta = 5000       # Porta que o Servidor esta
+
+host = '127.0.0.1'  # Endereço IP do Servidor
+port = 5000         # Porta que o Servidor está
 soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-origem = (host, porta)
+origem = (host, port)
 soquete.bind(origem)
-soquete.listen(0)
+soquete.listen(1)  # Escuta por 1 conexão de cada vez
+
+print("Servidor ouvindo...")
+
 while True:
     conexao, cliente = soquete.accept()
-    print 'Concetado por', cliente
-    mensagem = conexao.recv(1024)
-    print "Cliente ", cliente[0], "Recebida: ", mensagem
-    conexao.close()
-
+    print("Conectado por", cliente)
     
+    mensagem = conexao.recv(1024).decode()  # Decodifica a mensagem de bytes
+    print("Cliente", cliente[0], "Recebida:", mensagem)
+    
+    conexao.close()
